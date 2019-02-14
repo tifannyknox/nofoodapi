c0545cab3a6ef466745655c42676dd1e04fc15ac
Danubia <danyssoares@hotmail.com>
2019-02-14 17:56:12 -0200

    Sucesso

--- Nofood.Api/controllers/category.controller.js
+++ Nofood.Api/controllers/category.controller.js
@@ -1,5 +1,6 @@
 'use strict'
 
+require('../models/category.model');
 const mongoose = require('mongoose');
 const categoria = mongoose.model('Categoria');
 
@@ -8,21 +9,26 @@ function categoryController() {
 }
 
 categoryController.prototype.post = async (req, res) => {
-    let nova = new categoria(req.body);
-    return nova.save();
+    let modelo = new categoria(req.body);
+    let resultado = await modelo.save();
+    res.status(201).send(resultado);
 };
 categoryController.prototype.put = async (req, res) => {
     await categoria.findByIdAndUpdate(req.params.id, {$set: req.body});
-    return categoria.findById(req.params.id);
+    let categoriaEncontrada = await categoria.findById(req.params.id);
+    res.status(202).send(categoriaEncontrada);
 };
 categoryController.prototype.get = async (req, res) => {
-    return categoria.find();
+    let lista = await categoria.find();
+    res.status(200).send(lista);
 };
 categoryController.prototype.getById = async (req, res) => {
-    return categoria.findById(req.params.id);
+    let categoriaEncontrada = await categoria.findById(req.params.id)
+    res.status(200).send(categoriaEncontrada);
 };
 categoryController.prototype.delete = async (req, res) => {
-    return categoria.findByIdAndRemove(req.params.id);
+    let deletado = await categoria.findByIdAndRemove(req.params.id);
+    res.status(204).send(deletado);
 };
 
 module.exports = categoryController;
